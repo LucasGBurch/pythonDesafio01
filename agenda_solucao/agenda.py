949a4fbbb4869e1b1ffa76310b8e6b08b09869e0
Lucas Burch <lucasburch@hotmail.com>
2024-02-23 10:01:14 -0300

    Métodos de adicionar e visualizar normal
    Primeiros dois métodos do desafio prontos.

--- agenda_solucao/agenda.py
+++ agenda_solucao/agenda.py
@@ -1,4 +1,25 @@
+# SEÇÃO DOS MÉTODOS
+def adicionar_contato(contacts, contact_name, contact_phone, contact_email):
+  contact = {"Nome": contact_name, "Telefone": contact_phone, "E-mail": contact_email, "Favorite": False}
+  contacts.append(contact)
+  print(f"\nContato {contact_name} foi adicionado com sucesso!")
+  return
 
+def visualizar_contatos(contacts):
+  if len(contacts) > 0:
+    for indice, contact in enumerate(contacts, start=1):
+      contact_name = contact["Nome"]
+      contact_phone = contact["Telefone"]
+      contact_email = contact["E-mail"]
+      favorite = "Sim" if contact["Favorite"] else "Não"
+      print(f"{indice} - Nome: {contact_name} - Telefone: {contact_phone} - E-mail: {contact_email} - Favorito: {favorite}")
+  else:
+    print("Não há contatos na agenda. Tente adicionar novos.")
+
+# ------------------------------------------------------
+# SEÇÃO DE EXECUÇÃO DA AGENDA:
+
+contacts = []
 
 while True:
   print("\nBem-vindo à agenda Python! Suas opções:")
@@ -13,9 +34,12 @@ while True:
   choosen_option = input("Digite o número da opção: ")
 
   if choosen_option == '1':
-    print(f"Opção {choosen_option} escolhida.")
+    contact_name = input("Digite o nome do contato: ")
+    contact_phone = input("Digite o número de telefone do contato: ")
+    contact_email = input("Digite o e-mail do contato: ")
+    adicionar_contato(contacts, contact_name, contact_phone, contact_email)
   elif choosen_option == '2':
-    print(f"Opção {choosen_option} escolhida.")
+    visualizar_contatos(contacts)
   elif choosen_option == '3':
     print(f"Opção {choosen_option} escolhida.")
   elif choosen_option == '4':
@@ -27,3 +51,5 @@ while True:
   elif choosen_option == '7':
     print("Agenda encerrada. Até a próxima! o/")
     break
+  else:
+    print("Opção inválida. Tente novamente.")
