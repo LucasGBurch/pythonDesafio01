5a5986e5f73e918159bf4bbfad5e557f44cdf526
Lucas Burch <lucasburch@hotmail.com>
2024-02-23 08:37:04 -0300

    Estrutura das opções
    Estrutura inicial do projeto antes de implementar as alternativas.

---
+++ agenda_solucao/agenda.py
@@ -0,0 +1,29 @@
+
+
+while True:
+  print("\nBem-vindo à agenda Python! Suas opções:")
+  print("1. Adicionar contato")
+  print("2. Visualizar contatos cadastrados")
+  print("3. Editar um contato")
+  print("4. Marcar ou desmarcar contato como favorito")
+  print("5. Visualizar contatos favoritos")
+  print("6. Deletar um contato")
+  print("7. Encerrar agenda")
+
+  choosen_option = input("Digite o número da opção: ")
+
+  if choosen_option == '1':
+    print(f"Opção {choosen_option} escolhida.")
+  elif choosen_option == '2':
+    print(f"Opção {choosen_option} escolhida.")
+  elif choosen_option == '3':
+    print(f"Opção {choosen_option} escolhida.")
+  elif choosen_option == '4':
+    print(f"Opção {choosen_option} escolhida.")
+  elif choosen_option == '5':
+    print(f"Opção {choosen_option} escolhida.")
+  elif choosen_option == '6':
+    print(f"Opção {choosen_option} escolhida.")
+  elif choosen_option == '7':
+    print("Agenda encerrada. Até a próxima! o/")
+    break
