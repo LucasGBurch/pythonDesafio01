7f7f7eb96dbbefc46f317a1af57f90cc073d849f
burchlg <85189741@bat.com>
2024-02-23 13:57:43 -0300

    Método de edição
    Método de edição e os tratamentos de inputs.

--- agenda_solucao/agenda.py
+++ agenda_solucao/agenda.py
@@ -1,11 +1,11 @@
 # SEÇÃO DOS MÉTODOS
-def adicionar_contato(contacts, contact_name, contact_phone, contact_email):
+def add_contact(contacts, contact_name, contact_phone, contact_email):
   contact = {"Nome": contact_name, "Telefone": contact_phone, "E-mail": contact_email, "Favorite": False}
   contacts.append(contact)
   print(f"\nContato {contact_name} foi adicionado com sucesso!")
   return
 
-def visualizar_contatos(contacts):
+def list_contacts(contacts):
   if len(contacts) > 0:
     for indice, contact in enumerate(contacts, start=1):
       contact_name = contact["Nome"]
@@ -13,8 +13,27 @@ def visualizar_contatos(contacts):
       contact_email = contact["E-mail"]
       favorite = "Sim" if contact["Favorite"] else "Não"
       print(f"{indice} - Nome: {contact_name} - Telefone: {contact_phone} - E-mail: {contact_email} - Favorito: {favorite}")
+    return True
   else:
     print("Não há contatos na agenda. Tente adicionar novos.")
+    return False
+
+def edit_contacts(contacts, contact_number):
+
+  contact_index = int(contact_number) - 1
+
+  if contact_index >= 0 and contact_index < len(contacts): 
+    edit = input("Digite o que você gostaria de editar\nNome, Telefone ou E-mail: ")
+    if edit == "Nome" or edit == "Telefone" or edit == "E-mail":
+      change = input(f"Digite o novo {edit}: ")
+      contacts[contact_index][edit] = change
+      print(f"{edit} do contato modificado para {change}.")
+      return
+    else:
+      print("Atributo inválido.")
+  else:
+    print("Você deve escolher um número existente na lista.")
+  return # Caso não haja contatos, só virá a mensagem do list_contacts
 
 # ------------------------------------------------------
 # SEÇÃO DE EXECUÇÃO DA AGENDA:
@@ -37,11 +56,13 @@ while True:
     contact_name = input("Digite o nome do contato: ")
     contact_phone = input("Digite o número de telefone do contato: ")
     contact_email = input("Digite o e-mail do contato: ")
-    adicionar_contato(contacts, contact_name, contact_phone, contact_email)
+    add_contact(contacts, contact_name, contact_phone, contact_email)
   elif choosen_option == '2':
-    visualizar_contatos(contacts)
+    list_contacts(contacts)
   elif choosen_option == '3':
-    print(f"Opção {choosen_option} escolhida.")
+    if list_contacts(contacts):
+      contact_number = input("Digite o número do contato na lista: ")
+      edit_contacts(contacts, contact_number)
   elif choosen_option == '4':
     print(f"Opção {choosen_option} escolhida.")
   elif choosen_option == '5':
